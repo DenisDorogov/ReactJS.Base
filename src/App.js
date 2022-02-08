ad15030846008a37bf7bbeff4ab57932bf27bf4a
DenisDorogov <denisdorogov@yandex.ru>
2022-02-08 15:22:12 +0500

    Добавли ещё один компонент Technologies 10 урок.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 // import Header from './Header.js'
 import Header from './Header'
+import Technologies from './Technologies';
 
 const App = () => {
   return (
     <div className="App">
       < Header/>
+      < Technologies/>
     </div>
   );
 }
---
+++ src/Technologies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (<ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+    </ul>
+    )
+}
+
+export default Technologies;
