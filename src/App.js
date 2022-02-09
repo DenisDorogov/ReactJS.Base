89cc938b10015b9027b5582c8c677463da3f31d1
DenisDorogov <denisdorogov@yandex.ru>
2022-02-09 13:36:42 +0500

    Изменил mainContent на Profile 12 урок.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 // import Header from './Header.js'
 import Header from './components/Header'
-import MainContent from './components/MainContent';
+import Profile from './components/Profile';
 import Sidebar from './components/Sidebar';
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
     <div className="app-wrapper">
       < Header/>
       < Sidebar/>
-      < MainContent/>
+      < Profile/>
     </div>
   );
 }
