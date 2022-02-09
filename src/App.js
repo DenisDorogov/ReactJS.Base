9dc856cf399f9283a1942920cd1f675b4b4fb882
DenisDorogov <denisdorogov@yandex.ru>
2022-02-09 13:31:51 +0500

    Перенём компоненты в папку /components. 12 урок.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 // import Header from './Header.js'
-import Header from './Header'
-import MainContent from './MainContent';
-import Sidebar from './Sidebar';
+import Header from './components/Header'
+import MainContent from './components/MainContent';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
