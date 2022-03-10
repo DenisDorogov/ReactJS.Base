aab93c93cdc9d424aad65618e8e9df54c1a039ac
DenisDorogov <denisdorogov@yandex.ru>
2022-03-10 21:35:36 +0500

    Оформлена секция dialogs. Урок 20.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import Post from "../Post/Post";
 import style from "./Dialogs.module.css";
+import { NavLink } from 'react-router-dom';
 
 
 const Dialogs = () => {
     return(
         <div className={style.feed}>
-            Dialogs
+            <div className={style.dialogs_list}>
+                <div className={style.board}>
+                    <NavLink to="/dialogs/name" className={style.name}>Name</NavLink>
+                </div>
+                <div className={style.board}>
+                    <NavLink to="/dialogs/name" className={style.name}>Name</NavLink>
+                </div>
+                <div className={style.board}>
+                    <NavLink to="/dialogs/name"  className={style.name}>Name</NavLink>
+                </div>
+               
+            </div>
+
+            <div className={style.dialog}>
+                <p className={style.title}>Name</p>
+                <div className={style.message_in}>
+                    <img src="" alt="ava" className={style.avatar} />
+                    <div className={style.message}>Hello! <br/>Hello<br/>Hell</div>
+                </div>
+                <div className={style.message_out}>
+                    <img src="" alt="ava" className={style.avatar} />
+                    <div className={style.message}>Hello! <br/>Hello<br/>Hell</div>
+                </div>
+
+            
+            </div>
            
+
         </div>
     );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,11 +5,11 @@ import style from './Sidebar.module.css';
 const Sidebar = () => {
     return (
         <nav className={style.nav}>
-            <NavLink to='/profile' className={style.item} /*activeClassName={style.active}*/>Profile</NavLink>
-            <NavLink to='/dialogs' className={style.item} activeClassName={style.active}>Dialogs</NavLink>
-            <NavLink to='/news' className={style.item} activeClassName={style.active}>News</NavLink>
-            <NavLink to='/music' className={style.item} activeClassName={style.active}>Music</NavLink>
-            <NavLink to='/settings' className={style.item} activeClassName={style.active}>Settings</NavLink>
+            <NavLink to='/profile' className={style.item} >Profile</NavLink>
+            <NavLink to='/dialogs' className={style.item} >Dialogs</NavLink>
+            <NavLink to='/news' className={style.item} >News</NavLink>
+            <NavLink to='/music' className={style.item} >Music</NavLink>
+            <NavLink to='/settings' className={style.item} >Settings</NavLink>
         </nav>
     );
 }
