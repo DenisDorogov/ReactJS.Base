a1b0d8385a5ebd57536daf0050aecd28b73d5c7b
DenisDorogov <denisdorogov@yandex.ru>
2022-03-11 21:01:05 +0500

    В компонент поступает сразу массив с JSX разметкой. Массив маппится из ранее заготовненных массивов данных. Урок 25.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,61 +2,56 @@ import React from 'react';
 import style from "./Dialogs.module.css";
 import { NavLink } from 'react-router-dom';
 
-let img = "https://imgaz.staticbg.com/customers_avatars/20181219104152_503.jpg"
-
-let dialogs = [
-    { path: "/id1", name: "Name1" },
-    { path: "/id2", name: "Name2" },
-    { path: "/id3", name: "Name3" },
-    { path: "/id4", name: "Name4" },
-    { path: "/id5", name: "Name5" },
-];
-
-let messages = [
-    { src: img, alt: "ava1", message: "Hello!!!", in: true },
-    { src: img, alt: "ava1", message: "Hello", in: false },
-    { src: img, alt: "ava1", message: "Hell", in: true },
-    { src: img, alt: "ava1", message: "Hell!!!", in: true },
-    { src: img, alt: "ava1", message: "Hell", in: true },
-];
-
 const Board = (props) => {
     return (
         <div className={style.board}>
-            <NavLink to={"/dialogs" + props.dialog.path} className={style.name}>{props.dialog.name}</NavLink>
+            <NavLink to={"/dialogs" + props.path} className={style.name}>{props.name}</NavLink>
         </div>
     )
 }
 
 const Dialog = (props) => {
-    let inOut = props.message.in ? style.message_in : style.message_out;
+    let inOut = props.in ? style.message_in : style.message_out;
     return (<div className={inOut}>
-        <img src={props.message.src} alt={props.message.alt} className={style.avatar} />
-        <div className={style.message}>{props.message.message}</div>
+        <img src={props.src} alt={props.alt} className={style.avatar} />
+        <div className={style.message}>{props.message}</div>
     </div>
     )
 }
 
-
 const Dialogs = () => {
+
+    let img = "https://imgaz.staticbg.com/customers_avatars/20181219104152_503.jpg"
+
+    let dialogs = [
+        { path: "/id1", name: "Name1" },
+        { path: "/id2", name: "Name2" },
+        { path: "/id3", name: "Name3" },
+        { path: "/id4", name: "Name4" },
+        { path: "/id5", name: "Name5" },
+    ];
+
+    let messages = [
+        { src: img, alt: "ava1", message: "Hello!!!", in: true },
+        { src: img, alt: "ava1", message: "Hello", in: false },
+        { src: img, alt: "ava1", message: "Hell", in: true },
+        { src: img, alt: "ava1", message: "Hell!!!", in: true },
+        { src: img, alt: "ava1", message: "Hell", in: true },
+    ];
+
+    const boardList = dialogs.map(el => <Board path={el.path} name={el.name} />)
+
+    const messageList = messages.map(el => <Dialog src={el.src} alt={el.alt} message={el.message} in={el.in} />)
+
     return (
         <div className={style.feed}>
             <div className={style.dialogs_list}>
-                <Board dialog={dialogs[0]}/>
-                <Board dialog={dialogs[1]}/>
-                <Board dialog={dialogs[2]}/>
-                <Board dialog={dialogs[3]}/>
-                <Board dialog={dialogs[4]}/>
-               
+                {boardList}
             </div>
 
             <div className={style.dialog}>
                 <p className={style.title}>Name</p>
-                <Dialog message={messages[0]}/>
-                <Dialog message={messages[1]}/>
-                <Dialog message={messages[2]}/>
-                <Dialog message={messages[3]}/>
-                <Dialog message={messages[4]}/>
+                {messageList}
             </div>
         </div>
     );
--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import Post from "../Post/Post";
 import style from "./FeedPosts.module.css";
 
-
 const FeedPosts = () => {
+    
+    const posts = [
+        {name: 'Denis', text: 'test1', likeCount: 20},
+        {name: 'Denis1', text: 'test2', likeCount: 21},
+        {name: 'Denis2', text: 'test3', likeCount: 200},
+    ];
+
+    let postsList = posts.map( el => <Post name={el.name} text={el.text} likeCount={el.likeCount} />)
+
     return(
         <div className={style.feed}>
             <div>
-                <Post name={'Denis'} text="text 1" likeCount={30} /> 
-                <Post name={'Denis'} text="text 3" likeCount={30} />
-                <Post name={'Denis'} text="text 2" likeCount={30} />
+                {postsList}
             </div>
         </div>
     );
