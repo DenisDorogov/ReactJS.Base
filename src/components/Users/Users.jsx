7eed1d51db081be8d28fa2c4687cc689473766ea
DenisDorogov <denisdorogov@yandex.ru>
2022-04-10 12:18:07 +0500

    урок 51. Установил axios. Пользователи загружаются с сервера it-kamasutra.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,38 +1,47 @@
+import * as axios from 'axios';
 import React from 'react';
-import style from './users.module.css'
+import style from './users.module.css';
+import defaultPhoto from '../../assets/img/defaultUser.png';
 
 
 const Users = (props) => {
     console.log('props.users.length ' , props.users.length)
     let img = "https://imgaz.staticbg.com/customers_avatars/20181219104152_503.jpg"
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    img: img,
-                    followed: false,
-                    name: 'Petya',
-                    status: 'online',
-                    location: {city: 'St. Peterburg', country: 'Russia'}
-                },
-                {
-                    id: 2,
-                    img: img,
-                    followed: true,
-                    name: 'Vasya',
-                    status: 'offline',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    img: img,
-                    followed: false,
-                    name: 'Nikita',
-                    status: 'offline',
-                    location: {city: 'Murmansk', country: 'Russia'}
-                }
-            ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users?count=10')
+        .then(response => {
+            // debugger
+            props.setUsers(response.data.items)
+        });
+
+
+        // props.setUsers([
+        //         {
+        //             id: 1,
+        //             img: img,
+        //             followed: false,
+        //             name: 'Petya',
+        //             status: 'online',
+        //             location: {city: 'St. Peterburg', country: 'Russia'}
+        //         },
+        //         {
+        //             id: 2,
+        //             img: img,
+        //             followed: true,
+        //             name: 'Vasya',
+        //             status: 'offline',
+        //             location: {city: 'Moscow', country: 'Russia'}
+        //         },
+        //         {
+        //             id: 3,
+        //             img: img,
+        //             followed: false,
+        //             name: 'Nikita',
+        //             status: 'offline',
+        //             location: {city: 'Murmansk', country: 'Russia'}
+        //         }
+        //     ]
+        // )
     }
     
 
@@ -60,12 +69,13 @@ const Users = (props) => {
             {props.users.map((u) => { 
                 return (
                 <div className={style.userBox}>
-                    <img src={u.img} alt={u.name}/>
+                    <img src={u.photos.small != null ? u.photos.small : defaultPhoto} 
+                        alt={u.name}/>
                     <div className={style.userInfo}>
                         <p>{u.name}</p>
                         <p>{u.status}</p>
-                        <p>{u.location.city}</p>
-                        <p>{u.location.country}</p>
+                        {/* <p>{u.location.city}</p>
+                        <p>{u.location.country}</p> */}
                     </div>
                     {checkFollow(u.followed, u.id)}
                     {console.log('u: ', u)}
