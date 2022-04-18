b071204a5d239b1402dba239933b4cd2c95a4ed2
DenisDorogov <denisdorogov@yandex.ru>
2022-04-18 09:23:22 +0500

    60 урок. Для использования параметра из URL с id пользователя применён hoc withRouter. Пришлось откатить react-router-dom на 5 версию. И поменять сами роуты в App.js для работы в ней.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContaner from './components/Users/UsersContainer';
+import UsersContainer from './components/Users/UsersContainer';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import { Route, Routes } from 'react-router-dom';
@@ -15,9 +15,8 @@ const App = (props) => {
       < Header/>
       < Sidebar/>
       <div className={style.content}>
-                <Routes>
-
-                    <Route exact 
+                {/* <Routes> //Возврат к 5 версии react-router-dom*/} 
+                    {/* <Route exact 
                       path="/dialogs" 
                       element={<DialogsContainer />} 
                     />
@@ -25,11 +24,20 @@ const App = (props) => {
                       path="/profile" 
                       element={<ProfileContainer />} 
                     />
-                    <Route path="/users" element={<UsersContaner />} />
+                    <Route path="/users" element={<UsersContainer />} />
                     <Route path="/music" element={<Music />} />
-                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/settings" element={<Settings />} /> */}
 
-                </Routes>
+                {/* </Routes> */}
+
+                <Route path='/dialogs'
+                           render={ () => <DialogsContainer /> }/>
+
+                    <Route path='/profile/:userId?'
+                           render={ () => <ProfileContainer /> }/>
+
+                    <Route path='/users'
+                           render={ () => <UsersContainer /> }/>
             </div>
     </div>    
   );
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,12 +3,14 @@ import { connect } from 'react-redux';
 import Profile from './Profile';
 import {setUserProfile} from '../../redux/profile-reducer';
 import axios from 'axios';
+import {withRouter} from 'react-router-dom';
 
 class ProfileContainer extends React.Component {
 
     componentDidMount() {
         console.log('componentDidMount ProfileContainer props: ', this.props);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+        let userId = this.props.match.params.userId;
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
             .then(response => {
                 this.props.setUserProfile(response.data);
             });
@@ -25,5 +27,7 @@ let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 })
 
+let componentWithUrlData = withRouter(ProfileContainer)
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+
+export default connect(mapStateToProps, {setUserProfile})(componentWithUrlData);
